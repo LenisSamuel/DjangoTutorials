355e72c1aab8f5028af998b45ad74e42bbaf5b87
Samuel Lenis Mira <you@example.com>
2026-02-22 20:58:03 -0500

    Proyecto Django terminado con modelos, admin y comentarios

--- pages/urls.py
+++ pages/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('products/', ProductIndexView.as_view(), name='index'),
-    path('products/create/', ProductCreateView.as_view(), name='form'),
-    path('products/<str:id>/', ProductShowView.as_view(), name='show'),
+    path('products/create/', ProductCreateView.as_view(), name='create'),
+    path('products/<int:id>/', ProductShowView.as_view(), name='show'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,70 +1,63 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
-from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django import forms
+from django.core.exceptions import ValidationError
+from .models import Product
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
 
+
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update({
-            "title": "About us - Online Store",
-            "subtitle": "About Us",
-            "description": "This project was developed using Django.",
-            "author": "Developed by: Samuel Lenis",
-        })
-        return context
-
-class Product:
-    products = [
-        {"id": "1", "name": "TV", "description": "Best TV", "price": 200},
-        {"id": "2", "name": "iPhone", "description": "Best iPhone", "price": 900},
-        {"id": "3", "name": "Chromecast", "description": "Best Chromecast", "price": 50},
-        {"id": "4", "name": "Glasses", "description": "Best Glasses", "price": 30},
-    ]
 
 class ProductIndexView(View):
     template_name = 'products/index.html'
 
     def get(self, request):
-        viewData = {
-            "title": "Products - Online Store",
-            "subtitle": "List of products",
-            "products": Product.products
-        }
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] = "List of products"
+        viewData["products"] = Product.objects.all()
         return render(request, self.template_name, viewData)
+
 
 class ProductShowView(View):
     template_name = 'products/show.html'
 
     def get(self, request, id):
         try:
-            product = Product.products[int(id) - 1]
+            product_id = int(id)
+            if product_id < 1:
+                raise ValueError()
+            product = get_object_or_404(Product, pk=product_id)
         except:
             return HttpResponseRedirect(reverse('home'))
 
-        viewData = {
-            "title": product["name"] + " - Online Store",
-            "subtitle": product["name"] + " - Product Information",
-            "product": product
-        }
+        viewData = {}
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] = product.name + " - Product information"
+        viewData["product"] = product
         return render(request, self.template_name, viewData)
 
-class ProductForm(forms.Form):
+
+class ProductForm(forms.ModelForm):
     name = forms.CharField(required=True)
     price = forms.FloatField(required=True)
 
+    class Meta:
+        model = Product
+        fields = ['name', 'price']
+
     def clean_price(self):
-        price = self.cleaned_data["price"]
+        price = self.cleaned_data.get('price')
         if price <= 0:
-            raise forms.ValidationError("Price must be greater than 0")
+            raise ValidationError('Price must be greater than zero.')
         return price
+
 
 class ProductCreateView(View):
     template_name = 'products/create.html'
@@ -72,18 +65,16 @@ class ProductCreateView(View):
     def get(self, request):
         form = ProductForm()
         return render(request, self.template_name, {
-            "title": "Create Product",
+            "title": "Create product",
             "form": form
         })
 
     def post(self, request):
         form = ProductForm(request.POST)
         if form.is_valid():
-            return render(request, 'products/success.html', {
-                "title": "Product Created"
-            })
-
+            form.save()
+            return redirect('home')
         return render(request, self.template_name, {
-            "title": "Create Product",
+            "title": "Create product",
             "form": form
         })
