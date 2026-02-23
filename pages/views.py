cee86facde93ec9a2ff3bd2d13783fb5ad2eea2a
Samuel Lenis Mira <you@example.com>
2026-02-22 19:59:52 -0500

    Fix templates structure and products views

--- pages/views.py
+++ pages/views.py
@@ -1,23 +1,89 @@
+from django.views.generic import TemplateView
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django import forms
+
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About Us",
+            "description": "This project was developed using Django.",
+            "author": "Developed by: Samuel Lenis",
+        })
+        return context
 
 class Product:
     products = [
-        {"id":"1", "name":"TV", "description":"Best TV"},
-        {"id":"2", "name":"iPhone", "description":"Best iPhone"},
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
-        {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+        {"id": "1", "name": "TV", "description": "Best TV", "price": 200},
+        {"id": "2", "name": "iPhone", "description": "Best iPhone", "price": 900},
+        {"id": "3", "name": "Chromecast", "description": "Best Chromecast", "price": 50},
+        {"id": "4", "name": "Glasses", "description": "Best Glasses", "price": 30},
     ]
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {
+            "title": "Products - Online Store",
+            "subtitle": "List of products",
+            "products": Product.products
+        }
+        return render(request, self.template_name, viewData)
 
 class ProductShowView(View):
     template_name = 'products/show.html'
 
     def get(self, request, id):
-        product = Product.products[int(id)-1]
+        try:
+            product = Product.products[int(id) - 1]
+        except:
+            return HttpResponseRedirect(reverse('home'))
 
-        viewData = {}
-        viewData["title"] = product["name"]
-        viewData["subtitle"] = "Product information"
-        viewData["product"] = product
-
+        viewData = {
+            "title": product["name"] + " - Online Store",
+            "subtitle": product["name"] + " - Product Information",
+            "product": product
+        }
         return render(request, self.template_name, viewData)
+
+class ProductForm(forms.Form):
+    name = forms.CharField(required=True)
+    price = forms.FloatField(required=True)
+
+    def clean_price(self):
+        price = self.cleaned_data["price"]
+        if price <= 0:
+            raise forms.ValidationError("Price must be greater than 0")
+        return price
+
+class ProductCreateView(View):
+    template_name = 'products/create.html'
+
+    def get(self, request):
+        form = ProductForm()
+        return render(request, self.template_name, {
+            "title": "Create Product",
+            "form": form
+        })
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            return render(request, 'products/success.html', {
+                "title": "Product Created"
+            })
+
+        return render(request, self.template_name, {
+            "title": "Create Product",
+            "form": form
+        })
